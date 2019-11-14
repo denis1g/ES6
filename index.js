da21fe98beb55707b5e838ebd26f2d06cc618a83
denis_den <denis_gor@bk.ru>
2019-11-14 17:19:03 +0300

    17

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function Car(name) {
-	this.name = name
-}
+import $ from 'jquery'
+
+let promise = new Promise((resolve, reject) => {
+	$.ajax({
+		url: 'http://da2te.jsontest.com/',
+		dataType: 'json',
+		success: function (response) {
+			resolve(response)
+		},
+		error: function (error) {
+			reject('Error')
+		}
+	})
+})
+
+promise
+	.then((data) => {
+		return data.date
+	})
+	.then((date) => {
+		console.log('Date: ', date)
+	})
+	.catch((error) => {
+		console.info(error)
+		
+	})
